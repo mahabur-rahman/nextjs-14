cb8480f52b31a03e678adb77ae83b0c35c97bf4d
React | Next | Node <59826852+mahabur-rahman@users.noreply.github.com>
2023-12-31 19:07:26 +0600

    metadata

--- my-app/src/app/products/[productId]/page.tsx
+++ my-app/src/app/products/[productId]/page.tsx
@@ -1,9 +1,23 @@
-const ProductDetails = ({ params }: { params: { productId: string } }) => {
-  return (
-    <div>
-      <h2>Product details page: {params.productId}</h2>
-    </div>
-  );
+import { Metadata } from "next";
+
+type Props = {
+  params: { productId: string };
 };
 
-export default ProductDetails;
+export const generateMetadata = async ({
+  params,
+}: Props): Promise<Metadata> => {
+  const { productId } = params;
+  // const title = await new Promise((resolve) => {
+  //   setTimeout(() => {
+  //     resolve(productId);
+  //   }, 100);
+  // });
+  return {
+    title: `Product ${params.productId}`,
+  };
+};
+
+export default function ProductDetails({ params }: Props) {
+  return <h1>Details about product {params.productId}</h1>;
+}
