bf86a9765e692de299bfa17bc3636a8930aceaf3
mahabur-rahman <annur4395@gmail.com>
2024-10-28 13:01:18 +0600

    meatadata declare

--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,9 +1,23 @@
-const ProductDetails = ({ params: { productId } }: { params: { productId: string } }) => {
-  return (
-    <div>
-      Product Details: {productId}
-    </div>
-  );
+import { Metadata } from "next";
+
+type Props = {
+  params: { productId: string };
 };
 
-export default ProductDetails;
+export const generateMetadata = async ({
+  params,
+}: Props): Promise<Metadata> => {
+  const { productId } = params;
+  const title = await new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(productId);
+    }, 100);
+  });
+  return {
+    title: `Product ${title}`,
+  };
+};
+
+export default function ProductDetails({ params }: Props) {
+  return <h1>Details about product {params.productId}</h1>;
+}
