178c83eb1da9f79a433ff9fa5b992934fad386ee
mahabur-rahman <annur4395@gmail.com>
2024-10-29 15:37:54 +0600

    feat: last updated

--- src/app/api/comments/route.ts
+++ src/app/api/comments/route.ts
@@ -10,11 +10,15 @@ export async function GET() {
 }
 
 export async function POST(request: Request) {
-    const comment = await request.json();
-    const newComment = { id: comments.length + 1, text: comment.text };
-    comments.push(newComment);
-    return new Response(JSON.stringify(newComment), {
-      headers: { "Content-Type": "application/json" },
-      status: 201,
-    });
-  }
+  const comment = await request.json();
+
+
+  const newComment = { id: comments.length + 1, text: comment.text };
+
+  comments.push(newComment);
+
+  return new Response(JSON.stringify(newComment), {
+    headers: { "Content-Type": "application/json" },
+    status: 201,
+  });
+}
