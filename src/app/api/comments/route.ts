d364a3633f9885df122cd8b7029d7202605df91e
mahabur-rahman <annur4395@gmail.com>
2024-10-29 14:44:20 +0600

    API route handling with GET AND POST

---
+++ src/app/api/comments/route.ts
@@ -0,0 +1,20 @@
+import { comments } from "./data";
+
+export async function GET() {
+  return new Response(JSON.stringify(comments), {
+    status: 200,
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+}
+
+export async function POST(request: Request) {
+    const comment = await request.json();
+    const newComment = { id: comments.length + 1, text: comment.text };
+    comments.push(newComment);
+    return new Response(JSON.stringify(newComment), {
+      headers: { "Content-Type": "application/json" },
+      status: 201,
+    });
+  }
--- src/app/api/dashboard/route.ts
+++ src/app/api/dashboard/route.ts
@@ -1,6 +1,6 @@
 export async function GET() {
     return new Response(
-      JSON.stringify({ name: "test" }),
+      JSON.stringify({ name: "dashboard" }),
       { 
         status: 200, 
         headers: { 'Content-Type': 'application/json' } 
