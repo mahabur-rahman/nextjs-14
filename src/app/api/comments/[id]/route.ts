da89b61530ce5191903e4ab22a84b63d9e6c67b7
mahabur-rahman <annur4395@gmail.com>
2024-10-29 16:00:53 +0600

    search query params with filtering

---
+++ src/app/api/comments/[id]/route.ts
@@ -0,0 +1,29 @@
+import { comments } from "../data";
+
+// updated 
+export async function PATCH(
+    request: Request,
+    { params }: { params: { id: string } }
+  ) {
+    const body = await request.json();
+    const { text } = body;
+  
+    const index = comments.findIndex(
+      (comment) => comment.id === parseInt(params.id)
+    );
+    comments[index].text = text;
+    return Response.json(comments[index]);
+  }
+
+//   delete 
+  export async function DELETE(
+    _request: Request,
+    { params }: { params: { id: string } }
+  ) {
+    const index = comments.findIndex(
+      (comment) => comment.id === parseInt(params.id)
+    );
+    const deletedComment = comments[index];
+    comments.splice(index, 1);
+    return Response.json(deletedComment);
+  }
--- src/app/api/comments/route.ts
+++ src/app/api/comments/route.ts
@@ -1,14 +1,16 @@
+import { NextRequest } from "next/server";
 import { comments } from "./data";
 
-export async function GET() {
-  return new Response(JSON.stringify(comments), {
-    status: 200,
-    headers: {
-      "Content-Type": "application/json",
-    },
-  });
-}
+export async function GET(request: NextRequest) {
+    const searchParams = request.nextUrl.searchParams;
+    const query = searchParams.get("query");
+    const filteredComments = query
+      ? comments.filter((comment) => comment.text.includes(query))
+      : comments;
+    return Response.json(filteredComments);
+  }
 
+// post method 
 export async function POST(request: Request) {
   const comment = await request.json();
 
@@ -22,3 +24,6 @@ export async function POST(request: Request) {
     status: 201,
   });
 }
+
+
+
